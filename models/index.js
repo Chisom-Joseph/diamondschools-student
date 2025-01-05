3da0e3b5e627825d6d3e38ac6f10652f6258ceef
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2025-01-05 14:51:23 +0100

    Added disabled features database

--- models/index.js
+++ models/index.js
@@ -2,15 +2,24 @@ const dbConfig = require("../config/db.config");
 const { Sequelize, DataTypes } = require("sequelize");
 
 const Admin = require("./Admin");
+const Teacher = require("./Teacher");
 const Student = require("./Student");
 const Class = require("./Class");
+const Timetable = require("./Timetable");
 const Guardian = require("./Guardian");
 const Religion = require("./Religion");
 const Aspirant = require("./Aspirant");
+const DisabledFeatures = require("./DisabledFeature");
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
+  dialectOptions: {
+    connectTimeout: 100000,
+  },
+  define: {
+    timestamps: false,
+  },
   polli: {
     min: dbConfig.poll.min,
     max: dbConfig.poll.max,
@@ -25,12 +34,15 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 // DBs
+db.Admin = Admin(sequelize, DataTypes);
+db.Teacher = Teacher(sequelize, DataTypes);
 db.Student = Student(sequelize, DataTypes);
 db.Class = Class(sequelize, DataTypes);
+db.Timetable = Timetable(sequelize, DataTypes);
 db.Guardian = Guardian(sequelize, DataTypes);
-db.Admin = Admin(sequelize, DataTypes);
 db.Religion = Religion(sequelize, DataTypes);
 db.Aspirant = Aspirant(sequelize, DataTypes);
+db.DisabledFeatures = DisabledFeatures(sequelize, DataTypes);
 
 // Relations
 db.Student.belongsTo(db.Class);
@@ -47,5 +59,8 @@ db.Guardian.hasMany(db.Aspirant);
 
 db.Aspirant.belongsTo(db.Class);
 db.Class.hasMany(db.Aspirant);
+
+db.Timetable.belongsTo(db.Class);
+db.Class.hasMany(db.Timetable);
 
 module.exports = db;
