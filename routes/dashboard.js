15010b98382e5936597ba9e56a0f9ac116d76d87
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2025-01-25 13:18:58 +0100

    Updated dashoard route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -16,43 +16,12 @@ router.get("/profile", async (req, res) => {
 
 // Profile
 router.get("/timetables", async (req, res) => {
-  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-  const schedule = [
-    {
-      time: "8:00 AM - 9:00 AM",
-      subjects: ["Math", "Physics", "English", "Biology", "Chemistry"],
-    },
-    {
-      time: "9:00 AM - 10:00 AM",
-      subjects: ["English", "Math", "Physics", "Chemistry", "Biology"],
-    },
-    {
-      time: "10:00 AM - 11:00 AM",
-      subjects: ["Chemistry", "English", "Biology", "Math", "Physics"],
-    },
-    {
-      time: "11:00 AM - 12:00 PM",
-      subjects: ["Physics", "Biology", "Math", "English", "Chemistry"],
-    },
-    {
-      time: "12:00 PM - 1:00 PM",
-      subjects: [
-        "Lunch Break",
-        "Lunch Break",
-        "Lunch Break",
-        "Lunch Break",
-        "Lunch Break",
-      ],
-    },
-    {
-      time: "1:00 PM - 2:00 PM",
-      subjects: ["Biology", "Chemistry", "Math", "Physics", "English"],
-    },
-    {
-      time: "2:00 PM - 3:00 PM",
-      subjects: ["English", "Biology", "Chemistry", "Math", "Physics"],
-    },
-  ];
+  const { days, schedule } = await require("../utils/getTimetables")();
+
+  Object.entries(schedule).forEach(([key, value]) => {
+    console.log(key, value);
+    console.log(key, value[0]?.time);
+  });
 
   res.render("dashboard/timetables", {
     alert: req.flash("alert")[0] || "",
