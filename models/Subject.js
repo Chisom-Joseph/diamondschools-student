51395cfbc4aba0d6d64ebb7b4d7c5991d045f296
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2025-02-05 23:03:56 +0100

    Updated Subject db

--- models/Subject.js
+++ models/Subject.js
@@ -23,6 +23,37 @@ module.exports = (sequelize, DataTypes) => {
         notEmpty: true,
       },
     },
+    instructions: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    duration: {
+      type: DataTypes.FLOAT,
+      defaultValue: 30,
+      allowNull: true,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    date: {
+      type: DataTypes.STRING,
+      defaultValue: Date.now(),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
     deleted: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
